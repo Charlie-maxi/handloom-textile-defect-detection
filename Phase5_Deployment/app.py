e080e060a6bc7ab49ea9f0e23cd9c0df6740049a
Abhishek B V <abhishek2003bv@gmail.com>
2026-09-16 23:23:08 +0530

    Update app.py

--- Phase5_Deployment/app.py
+++ Phase5_Deployment/app.py
@@ -189,9 +189,10 @@ app = gr.mount_gradio_app(
 if __name__ == "__main__":
 
     import uvicorn
+    import os
 
     uvicorn.run(
         app,
-        host="127.0.0.1",
-        port=7860
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
     )
